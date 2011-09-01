989307659d7f8281ed2f708e37cbe00b20f889e8
Justin Thibault <jvthibault@gmail.com>
2011-09-01 13:58:38 -0700

    refactored a bit, cleaned up broken tables for long descriptions

--- lib/char.rb
+++ lib/char.rb
@@ -3,18 +3,21 @@ module Wingalingding
   
   class Char
     
-    attr_reader :description, :char, :hex, :code
+    attr_accessor :description, :char, :hex, :code
+    
+    # Truncate any character descriptions exceeding this length
+    MAX_DESCRIPTION_LENGTH = 90
       
     def self.find(description)
       Scraper.
-      character_map.
-      find_all{|char| char =~ Regexp.new(description, "xig") }.
-      map{|c| self.new(c)}
+        character_map.
+        find_all{|char| char =~ Regexp.new(description, "xig") }.
+        map{|c| self.new(c)}
     end
     
     def initialize(line)
       @stripped = line.strip.split(/\t/)
-      @description = @stripped[1]
+      @description = @stripped[1][0..MAX_DESCRIPTION_LENGTH]
       @char = @stripped[0]
       @hex = @stripped[2]
       @code = @description.match(/\(U\+[a-zA-Z0-9]+\)/).to_s
@@ -26,20 +29,11 @@ module Wingalingding
     
     def self.display(description)
       chars = self.find(description)
-      desc_max = chars.map{|c| c.description.size }.max
-      op = ""
-      op = "┏━━━━━┳━"
-      desc_max.times{ op << "━"}
-      op << "━━┓\n"
-      chars.each do |c|
-        op << "┃  #{c.char}  ┃  #{c.description.ljust(desc_max)} ┃\n"
-      end
-      op << "┗━━━━━┻━"
-      desc_max.times{ op << "━"}
-      op << "━━┛"
-      return op
+      longest_description_length = chars.map{|c| c.description.size }.max
+      table = "┏━━━━━┳━" + ("━" * longest_description_length) + "━━┓\n"
+      chars.each{|c| table << "┃  #{c.char}  ┃  #{c.description.ljust(longest_description_length)} ┃\n" } 
+      return table + "┗━━━━━┻━" + ( "━" * longest_description_length )  + "━━┛"
     end
-
     
   end
   
